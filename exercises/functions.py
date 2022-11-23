1df1d743e8bd7b6b75d59dca8a9c47653ba51b4e
Reaxp <92944859+Reaxp@users.noreply.github.com>
2022-11-24 00:07:48 +0300

    test

--- exercises/functions.py
+++ exercises/functions.py
@@ -4,7 +4,24 @@
 # display_message()
 
 # 8.2
-def favorite_book(name):
-    print(f"My favorite book is {name} ")
+# def favorite_book(name):
+#     print(f"My favorite book is {name} ")
+#
+# favorite_book("Эрик Мэтиз_Изучаем Python.")
 
-favorite_book("Эрик Мэтиз_Изучаем Python.")
+# 8.3
+# def make_shirt(size, text):
+#     print(f"Вы заказали футболку {size} размера с надписью - '{text.title()}'.")
+# make_shirt("52","hello world")
+
+# 8.4
+# def make_shirt(size="L", text="I love Python"):
+#     print(f"Вы заказали футболку {size} размера с надписью - '{text.title()}'.")
+# make_shirt()
+# make_shirt("52","hello world")
+
+# 8.5
+def discribe_city(city, country="Россия"):
+    print(f" {country.title()} великая страна. В ней есть город '{city.title()}'.")
+discribe_city("Москва")
+discribe_city("Владивосток")
---
+++ functions/pets.py
@@ -0,0 +1,21 @@
+# def describe_pet(animal_type, pet_mame):
+#     """Выводит информацию о животном"""
+#     print(f"\nУ меня дома есть {animal_type}")
+#     print(f"Моего {animal_type}а зовут {pet_mame.title()}.")
+#
+#
+# # describe_pet("кот", "мурзик")
+# # describe_pet("жираф", "марвин")
+#
+# describe_pet(animal_type="кот", pet_mame="мурзик")
+# describe_pet(animal_type="жираф", pet_mame="марвин")
+
+
+def describe_pet(animal_type, pet_mame):
+    """Выводит информацию о животном"""
+    print(f"\nУ меня дома есть {animal_type}")
+    print(f"Моего {animal_type}а зовут {pet_mame.title()}.")
+
+describe_pet()
+
+
