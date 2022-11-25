df50b43656628d4962d034a9b0b3b3536c0df546
Reaxp <92944859+Reaxp@users.noreply.github.com>
2022-11-25 14:30:53 +0300

    Необязательные аргументы

--- functions/formatted_name.py
+++ functions/formatted_name.py
@@ -1,7 +1,32 @@
-def get_formated_name(first_name, last_name):
-    """Возвращает аккуратно отформатированное имя."""
-    full_name = f"{first_name} {last_name}"
+# def get_formated_name(first_name, last_name):
+#     """Возвращает аккуратно отформатированное имя."""
+#     full_name = f"{first_name} {last_name}"
+#     return full_name.title()
+#
+# musician = get_formated_name("алекс", "reaexp")
+# print(musician)
+
+
+# def get_formated_name(first_name, middle_name, last_name):
+#     """Возвращает аккуратно отформатированное полное имя."""
+#     full_name = f"{first_name} {middle_name} {last_name}"
+#     return full_name.title()
+#
+#
+# musician = get_formated_name("алекс", "крутой", "reaexp")
+# print(musician)
+
+def get_formated_name(first_name, last_name, middle_name=''):
+    """Возвращает аккуратно отформатированное полное имя."""
+    if middle_name:
+        full_name = f"{first_name} {middle_name} {last_name}"
+    else:
+        full_name = f"{first_name} {last_name}"
     return full_name.title()
 
+
 musician = get_formated_name("алекс", "reaexp")
+print(musician)
+
+musician = get_formated_name("алекс", "крутой", "reaexp")
 print(musician)
