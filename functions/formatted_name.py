23fef0fa8825930c42844bab9d91b363a4f3d711
Reaxp <92944859+Reaxp@users.noreply.github.com>
2022-11-25 19:53:01 +0300

    Использование функции в цикле while

--- functions/formatted_name.py
+++ functions/formatted_name.py
@@ -16,17 +16,37 @@
 # musician = get_formated_name("алекс", "крутой", "reaexp")
 # print(musician)
 
-def get_formated_name(first_name, last_name, middle_name=''):
-    """Возвращает аккуратно отформатированное полное имя."""
-    if middle_name:
-        full_name = f"{first_name} {middle_name} {last_name}"
-    else:
-        full_name = f"{first_name} {last_name}"
-    return full_name.title()
+# def get_formated_name(first_name, last_name, middle_name=''):
+#     """Возвращает аккуратно отформатированное полное имя."""
+#     if middle_name:
+#         full_name = f"{first_name} {middle_name} {last_name}"
+#     else:
+#         full_name = f"{first_name} {last_name}"
+#     return full_name.title()
+#
+#
+# musician = get_formated_name("алекс", "reaexp")
+# print(musician)
+#
+# musician = get_formated_name("алекс", "крутой", "reaexp")
+# print(musician)
 
 
-musician = get_formated_name("алекс", "reaexp")
-print(musician)
+# def build_person(first_name, last_name):
+#     """Возвращает словарь с информацией о человеке."""
+#     person = {'first': first_name, 'last': last_name}
+#     return person
+#
+# musician = build_person('алекс', 'reaexp')
+# print(musician)
 
-musician = get_formated_name("алекс", "крутой", "reaexp")
+
+def build_person(first_name, last_name, age = None):
+    """Возвращает словарь с информацией о человеке."""
+    person = {'first': first_name, 'last': last_name}
+    if age:
+        person['age'] = age
+    return person
+
+musician = build_person('алекс', 'reaexp', age = 35 )
 print(musician)
--- functions/greeter.py
+++ functions/greeter.py
@@ -4,8 +4,28 @@
 #     print("Hello, Alex!")
 # greet_user()
 
-def greet_user(username):
-    """Выводит простое приветствие."""
-    print(f"Hello, {username.title()}!")
+# def greet_user(username):
+#     """Выводит простое приветствие."""
+#     print(f"Hello, {username.title()}!")
+#
+# greet_user("alex")
 
-greet_user("alex")
+
+def get_formatted_name(first_name, last_name):
+    """Возвращает аккуратно отформатированное полное имя."""
+    full_name = f"{first_name} {last_name}"
+    return full_name.title()
+
+
+while True:
+    print("\nПожалуйста, скажите свое имя и фамилию. ")
+    print("Напишите 'стоп' или 'stop' что бы остановить программу")
+    f_name = input("имя: ")
+    if f_name == "стоп" or f_name == "stop":
+        break
+    l_name = input("фамилия: ")
+    if f_name == "стоп" or f_name == "stop":
+        break
+
+    formated_name = get_formatted_name(f_name, l_name)
+    print(f"Привет {formated_name}")
