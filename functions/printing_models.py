b17a0f18454697c0e31ccf0f191a9c7cfdf565e0
Revit <reaexp.it@gmail.com>
2022-11-27 23:55:59 +0300

    Запрет изменения списка в функции

--- functions/printing_models.py
+++ functions/printing_models.py
@@ -12,6 +12,25 @@
 #     print(completed_mod)
 
 
+# unprintrd_disigns = ['phone case', 'robot pedant', 'dodecahedron']
+# completed_model = []
+#
+# def print_models(unprintrd_disigns, completed_model ):
+#     while unprintrd_disigns:
+#         current_design = unprintrd_disigns.pop()
+#         print(f"Printing model: {current_design}")
+#         completed_model.append(current_design)
+#
+#
+# def show_complited_models(completed_model):
+#     print("\nThe following models have been printed:")
+#     for completed_mod in completed_model:
+#         print(completed_mod)
+#
+# print_models(unprintrd_disigns, completed_model)
+# show_complited_models(completed_model)
+
+
 unprintrd_disigns = ['phone case', 'robot pedant', 'dodecahedron']
 completed_model = []
 
@@ -27,5 +46,7 @@ def show_complited_models(completed_model):
     for completed_mod in completed_model:
         print(completed_mod)
 
-print_models(unprintrd_disigns, completed_model)
+print_models(unprintrd_disigns[:], completed_model) # [:] создает копию списка для передачи функции
 show_complited_models(completed_model)
+print(unprintrd_disigns)
+print(completed_model)
