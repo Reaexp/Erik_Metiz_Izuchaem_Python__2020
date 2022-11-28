4bf85467cea534d7a52cd1ca36987844cca03604
Revit <reaexp.it@gmail.com>
2022-11-28 11:47:22 +0300

    Позиционные аргументы с произвольными наборами аргументовв

--- functions/pizza.py
+++ functions/pizza.py
@@ -3,9 +3,17 @@
 # make_pizza('peperoni')
 # make_pizza('mushrooms', 'green peppers', 'extra cheese')
 
-def make_pizza(*topings):
-    print("Making pizzas with the following topings: ")
+# def make_pizza(*topings):
+#     print("Making pizzas with the following topings: ")
+#     for toping in topings:
+#         print(f' - {toping}')
+# make_pizza('peperoni')
+# make_pizza('mushrooms', 'green peppers', 'extra cheese')
+
+
+def make_pizza(size, *topings):
+    print(f"Making {size} with the following topings: ")
     for toping in topings:
         print(f' - {toping}')
-make_pizza('peperoni')
-make_pizza('mushrooms', 'green peppers', 'extra cheese')
+make_pizza(16, 'peperoni')
+make_pizza(23, 'mushrooms', 'green peppers', 'extra cheese')
