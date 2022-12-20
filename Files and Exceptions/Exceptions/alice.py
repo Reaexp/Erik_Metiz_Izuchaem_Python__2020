554ff2e53b8af14a6a66846b324cd871582597e7
Revit <reaexp.it@gmail.com>
2022-12-20 22:11:30 +0300

    23:49

---
+++ Files and Exceptions/Exceptions/alice.py
@@ -0,0 +1,45 @@
+# filename = 'alice.txt'
+#
+# with open(filename, encoding='utf-8') as f:
+#     contents = f.read()
+
+# filename = 'alice.txt'
+# try:
+#     with open(filename, encoding='utf-8') as f:
+#         contents = f.read()
+# except FileNotFoundError:
+#     print(f"Sorry, the file '{filename}' does not exist.")
+
+# filename = 'alice.txt'
+# try:
+#     with open(filename, encoding='utf-8') as f:
+#         contents = f.read()
+# except FileNotFoundError:
+#     print(f"Sorry, the file '{filename}' does not exist.")
+#
+# else:
+#     # Подсчитаем приблизительное количество строк в файле
+#     words = contents.split()
+#     num_words = len(words)
+#     print(f"The file '{filename}' has about {num_words} words.")
+
+def count_words(filename):
+    """ Подсчитаем приблизительное количество строк в файле"""
+    try:
+        with open(filename, encoding='utf-8') as f:
+            contents = f.read()
+    except FileNotFoundError:
+        print(f"Sorry, the file '{filename}' does not exist.")
+
+    else:
+
+        words = contents.split()
+        num_words = len(words)
+        print(f"The file '{filename}' has about {num_words} words.")
+
+# filename = 'alice.txt'
+# count_words(filename)
+
+filenames = ['alice.txt', 'siddhartha.txt', 'moby_dick.txt', 'little_women.txt' ]
+for filename in filenames:
+    count_words(filename)
